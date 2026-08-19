9abfda1ef7295d225e15fca83466b92f6fc0d17e
melisa aslan <160237310+melisaaslan311@users.noreply.github.com>
2026-08-19 14:34:24 +0300

    Update embedding.py

--- embeddings/embedding.py
+++ embeddings/embedding.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import HuggingFaceEmbeddings
+
 embedding = HuggingFaceEmbeddings(
-    model_name="intfloat/multilingual-e5-base",
+    model_name="intfloat/multilingual-e5-large",
     model_kwargs={"device": "cpu"},
     encode_kwargs={"normalize_embeddings": True},
 )
